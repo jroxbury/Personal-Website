e7835733777f11d3c675f6c8245079bb58a4a03d
Joe <Joseph.Roxbury@arteric.com>
2017-01-15 21:59:17 -0500

    adding checkers

--- personal.js
+++ personal.js
@@ -38,6 +38,11 @@ myApp.config(function ($routeProvider) {
 		controller:'portfolioController'
 	})
 
+  .when('/portfolio4', {
+    templateUrl:'pages/portfolio4.html',
+    controller:'portfolioController'
+  })
+
 
 });
 
